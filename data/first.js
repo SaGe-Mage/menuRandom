0c8fe28c4b4017af11cf5ea7c364974eba73b654
Sergey Moskvitin <moskvitin.sergei@yandex.ru>
2021-06-22 15:17:33 +0300

    refactor

---
+++ data/first.js
@@ -0,0 +1,19 @@
+export const firstList = [
+  {name: 'Жульен', isIndependent: true},
+  {name: 'Паста Карбонара', isIndependent: true},
+  {name: 'ВОК Лапша', isIndependent: true},
+  {name: 'ВОК Рис', isIndependent: true},
+  {name: 'Фетучини', isIndependent: true},
+  {name: 'Рагу', isIndependent: true},
+  {name: 'Плов', isIndependent: true},
+  {name: 'Гырдбзя', isIndependent: true},
+  {name: 'Тыква по-гречески', isIndependent: true},
+  {name: 'Рис', isIndependent: false},
+  {name: 'Макароны', isIndependent: false},
+  {name: 'Греча', isIndependent: false},
+  {name: 'Булгур', isIndependent: false},
+  {name: 'Перловка', isIndependent: false},
+  {name: 'Картошка', isIndependent: false},
+  {name: 'Горох', isIndependent: false},
+  {name: 'Капуста', isIndependent: false}
+];
--- script.js
+++ script.js
@@ -1,54 +1,27 @@
+import {firstList} from './data/first.js';
+import {secondList} from './data/second.js';
+
 const button = document.querySelector(".button");
 const firstLine = document.querySelector(".lineFirst");
 const secondLine = document.querySelector(".lineSecond");
-const firstList = [
-  {name: 'Жульен', isIndependent: true},
-  {name: 'Паста Карбонара', isIndependent: true},
-  {name: 'ВОК Лапша', isIndependent: true},
-  {name: 'ВОК Рис', isIndependent: true},
-  {name: 'Фетучини', isIndependent: true},
-  {name: 'Рагу', isIndependent: true},
-  {name: 'Плов', isIndependent: true},
-  {name: 'Гырдбзя', isIndependent: true},
-  {name: 'Тыква по-гречески', isIndependent: true},
-  {name: 'Рис', isIndependent: false},
-  {name: 'Макароны', isIndependent: false},
-  {name: 'Греча', isIndependent: false},
-  {name: 'Булгур', isIndependent: false},
-  {name: 'Перловка', isIndependent: false},
-  {name: 'Картошка', isIndependent: false},
-  {name: 'Горох', isIndependent: false},
-  {name: 'Капуста', isIndependent: false}
-];
-const secondList = [
-  'Рыба',
-  'Котлета',
-  'Пердечки',
-  'Мясо',
-  'Курица',
-  'Сосиски',
-  'Тушёнка',
-  'Килька в томатном соусе'
-];
 
-function rend(list, currentValue) {
-  let a = Math.floor(Math.random() * list.length);
+function randomize(list, currentValue) {
+  let index = Math.floor(Math.random() * list.length);
 
-  if (list[a] === currentValue || list[a].name === currentValue) {
-    return rend(list, currentValue);
-  } else {
-    return a;
+  if (list[index] === currentValue || list[index].name === currentValue) {
+    return randomize(list, currentValue);
   }
+    return index;
 }
 
 button.addEventListener("click", (event) => {
   event.preventDefault();
-  let n = firstList[rend(firstList, firstLine.textContent)];
-  if (n.isIndependent) {
-    firstLine.textContent = n.name;
+  let dish = firstList[randomize(firstList, firstLine.textContent)];
+  if (dish.isIndependent) {
+    firstLine.textContent = dish.name;
     secondLine.textContent = 'Хватит';
   } else {
-    firstLine.textContent = n.name;
-    secondLine.textContent = secondList[rend(secondList, secondLine.textContent)];
+    firstLine.textContent = dish.name;
+    secondLine.textContent = secondList[randomize(secondList, secondLine.textContent)];
   }
 })
